fb5b149d8533764ece641b6018a741356174fcc7
Alex <lxvd@bk.ru>
2020-01-20 15:27:33 +0300

    First commit

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:4.0
+import PackageDescription
+
+let package = Package(
+    name: "Hello",
+    products: [
+        .library(name: "Hello", targets: ["App"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(name: "App", dependencies: ["FluentSQLite", "Vapor"]),
+        .target(name: "Run", dependencies: ["App"]),
+        .testTarget(name: "AppTests", dependencies: ["App"])
+    ]
+)
+
